3c3d9c031ab8ae8a53ee9c941426d29a67c00d94
sethearn1234 <gamedevsignup@gmail.com>
2015-12-25 23:03:01 -0800

    fixed some stuff
    added a character and fixed some stuff

---
+++ Assets/knudsjef/PlayerAnim.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+var animator : Animator;
+var v : float;
+var h : float;
+var sprint : float;
+function Start () {
+	animator = GetComponent(Animator);
+}
+
+function Update () {
+	v = Input.GetAxis("Vertical");
+	h = Input.GetAxis("Horizontal");
+	Sprinting();
+}
+function FixedUpdate(){
+	animator.SetFloat("Walk", v);
+	animator.SetFloat("Turn", h);
+	animator.SetFloat("Sprint", sprint);
+}
+function Sprinting(){
+	if(Input.GetKeyDown(KeyCode.Z) && Input.GetKeyDown(KeyCode.W)){
+		sprint = 0.2;
+	}else {
+		sprint = 0.0;
+	}
+}
