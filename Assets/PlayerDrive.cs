bfb41b78895f893267c9eeff8873337a7d245789
sethearn1234 <gamedevsignup@gmail.com>
2015-12-28 17:20:38 -0800

    made some big changes(added driving)
    added driving to game

---
+++ Assets/PlayerDrive.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace UnityStandardAssets.Vehicles.Car
+{
+    public class PlayerDrive : MonoBehaviour
+    {
+        //player gameobjects
+        public CarUserControl car2;
+        
+        public GameObject player;
+        public GameObject car;
+        void Start()
+        {
+            car2 = gameObject.GetComponent<CarUserControl>();
+            car2.enabled = false;
+        }
+        void OnTriggerEnter(Collider col)
+        {
+          
+            if (col.CompareTag("Player"))
+            {
+              
+                    Debug.Log("enter");
+                    player.transform.parent = car.transform;
+                    player.SetActive(false);
+                    car2.enabled = true;
+            
+            }
+
+        }
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                player.transform.parent = null;
+                player.SetActive(true);
+                car2.enabled = false;
+                
+            }
+        }
+
+    }
+}
+
---
+++ Assets/hidecursor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hidecursor : MonoBehaviour {
+	bool s = false;
+	// Use this for initialization
+	void Start () {
+		Screen.lockCursor = true;
+		Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (s == false) {
+			Screen.lockCursor = true;
+			Cursor.visible = false;
+		}
+		if (s == true) {
+			Screen.lockCursor = false;
+			Cursor.visible = true;
+		}
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			s = true;
+		}
+		if (Input.GetKeyDown (KeyCode.F1)) {
+			s = false;
+		}
+	}
+}
