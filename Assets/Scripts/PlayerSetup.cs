dd5f4bc7d05ffa39696a45e5d60fd4421b882389
sethearn1234 <gamedevsignup@gmail.com>
2016-01-01 16:54:50 -0800

    added some new stuff
    added some new stuff

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] DisabledObjects;
+    
+    Camera SceneCamera;
+    void Start()
+    {
+        
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < DisabledObjects.Length; i++)
+            {
+                DisabledObjects[i].enabled = false;
+            }
+        }
+        else
+        {
+            SceneCamera = Camera.main;
+            if(SceneCamera != null)
+            {
+                SceneCamera.gameObject.SetActive(false);
+            }
+            
+        }
+        GetComponent<NetworkAnimator>().SetParameterAutoSend(0, true);
+    }
+
+    public override void PreStartClient()
+    {
+        GetComponent<NetworkAnimator>().SetParameterAutoSend(0, true);
+    }
+
+    void OnDisable()
+    {
+        if(SceneCamera != null)
+        {
+            SceneCamera.gameObject.SetActive(true);
+        }
+    }
+
+}
